0745cf139d76e1521951d8170589fd146fc61302
Cristian Pop <Cristi075@ymail.com>
2016-12-15 00:06:13 +0200

    Implemented parsing for signs

--- parse.py
+++ parse.py
@@ -8,45 +8,53 @@ from programs import program0
 
 patternDriver = r'Driver\(([^,]*),([^,]*),([^,]*),([^,]*)\)'
 patternRoad = r'Road\(([^,]*),([^,]*),([^,]*),([^,]*)\)'
+patternSign = r'Sign\(([^,]*),([^,]*)\)'
 
 def parse(inputFile,outputFile):
 	env=Crossroad()	
 	for line in inputFile.readlines():
-		res = re.match(patternDriver,line.strip())
-		# This line describes a driver
-		if(res != None):
-			name=res.group(1)
-			current_road=res.group(2)
-			destination_group=res.group(3)
-			if(res.group(4)=='true'):
+		resDriver 	= re.match(patternDriver,line.strip())
+		resRoad   	= re.match(patternRoad,line.strip())
+		resSign		= re.match(patternSign,line.strip())
+		if(resDriver != None): # This line describes a driver
+			name=resDriver.group(1)
+			current_road=resDriver.group(2)
+			destination_group=resDriver.group(3)
+			if(resDriver.group(4)=='true'):
 				emergency=True
 			else:
 				emergency=False
 			env.add_thing(Driver(program0,name,current_road,destination_group,emergency))
-
-		res = re.match(patternRoad,line.strip())
-		# This line describes a road
-		if(res != None):
-			name=res.group(1)
-			leftName=res.group(2).strip()
+		elif(resRoad != None): # This line describes a road
+			name=resRoad.group(1)
+			leftName=resRoad.group(2).strip()
 			if (leftName == ''):
 				left=[]
 			else:
 				left=[leftName]
 
-			forwardName=res.group(3).strip()
+			forwardName=resRoad.group(3).strip()
 			if (forwardName == ''):
 				forward=[]
 			else:
 				forward=[forwardName]
 
-			rightName=res.group(4).strip()
+			rightName=resRoad.group(4).strip()
 			if (rightName == ''):
 				right=[]
 			else:
 				right=[rightName]
 
 			env.add_thing(Road(name,left,forward,right))
+		elif(resSign != None):
+			name=resSign.group(1)
+			road=resSign.group(2)
+			env.add_thing(Sign(name,road))
+			print(Sign(name,road))
+		else:
+			if(line.strip()!=''):
+				print('The following line did not match any of the existing patterns:')
+				print(line)
 	inputFile.close()
 	print('Environment created. Running checks')
 	if(check(env)):
